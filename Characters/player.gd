459ec65d194c214037c9487149d4ff5d2c3b4f55
Eddy Naddeo <eddy@naddeo.ch>
2023-10-31 10:10:39 +0100

    Multiplayer working base

--- Characters/player.gd
+++ Characters/player.gd
@@ -11,51 +11,52 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var tile_map : TileMap = $"../TileMap"
 
 func _physics_process(delta):
-	if (position.y > 512):
-		kill()
-	animated_sprite.play()
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if (velocity.y <= 0):
-			animated_sprite.animation = "jump"
-		else:
-			animated_sprite.animation = "fall"
-	else:
-		if(velocity.x == 0):
-			animated_sprite.animation = "idle"
-		else:
-			animated_sprite.animation = "run"
-			
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-		
-		if (velocity.x < 0):
-			animated_sprite.flip_h = true
-		elif (velocity.x > 0):
-			animated_sprite.flip_h = false
-		
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var tile_map_pos = tile_map.local_to_map(Vector2i(position.x, position.y))
-
-	var tile_data : TileData = tile_map.get_cell_tile_data(0, Vector2i(tile_map_pos.x,tile_map_pos.y+1))
-
-	if tile_data:
-		var current_data = tile_data.get_custom_data("dead")
-		
-		if (current_data):
+	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
+		if (position.y > 512):
 			kill()
+		animated_sprite.play()
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			if (velocity.y <= 0):
+				animated_sprite.animation = "jump"
+			else:
+				animated_sprite.animation = "fall"
+		else:
+			if(velocity.x == 0):
+				animated_sprite.animation = "idle"
+			else:
+				animated_sprite.animation = "run"
+				
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	move_and_slide()
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+			
+			if (velocity.x < 0):
+				animated_sprite.flip_h = true
+			elif (velocity.x > 0):
+				animated_sprite.flip_h = false
+			
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+		var tile_map_pos = tile_map.local_to_map(Vector2i(position.x, position.y))
+
+		var tile_data : TileData = tile_map.get_cell_tile_data(0, Vector2i(tile_map_pos.x,tile_map_pos.y+1))
+
+		if tile_data:
+			var current_data = tile_data.get_custom_data("dead")
+			
+			if (current_data):
+				kill()
+
+		move_and_slide()
 
 func kill():
 	velocity.x = 0
@@ -64,3 +65,6 @@ func kill():
 
 func _on_kill_pressed():
 	kill()
+
+func _ready():
+	$MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
--- Levels/level.gd
+++ Levels/level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var tile_map = $TileMap
 
+@export var PlayerScene : PackedScene
+
 var ground_layer = 0
 
 var source_id = 0
@@ -13,6 +15,15 @@ var bloc_coord = Vector2i(12,9)
 var tile_map_pos = Vector2i(0,0)
 
 func _ready():
+	var index = 0
+	for i in GameManager.Players:
+		var currentPlayer = PlayerScene.instantiate()
+		currentPlayer.name = str(GameManager.Players[i].id)
+		add_child(currentPlayer)
+		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoint"):
+			if spawn.name == str(index):
+				currentPlayer.global_position = spawn.global_position
+		index += 1
 	pass
 
 func _on_button_pressed():
@@ -33,6 +44,14 @@ func _input(event):
 			tile_map_pos = tile_map.local_to_map(mouse_pos)
 			
 			if (bloc_coord == Vector2i(99,99)):
-				tile_map.erase_cell(ground_layer, tile_map_pos)
+				rpc("rpc_erase", ground_layer, tile_map_pos)
 			else:
-				tile_map.set_cell(ground_layer, tile_map_pos, source_id, bloc_coord)
+				rpc("rpc_place", ground_layer, tile_map_pos, source_id, bloc_coord)
+
+@rpc("any_peer", "call_local")
+func rpc_erase(layer, pos):
+	tile_map.erase_cell(layer, pos)
+
+@rpc("any_peer", "call_local")
+func rpc_place(layer, pos, id, coord):
+	tile_map.set_cell(layer, pos, id, coord)
