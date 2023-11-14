4129fa3e9746dc14d43e7dc6f06328d002cc2133
Mendes Sébastien <sebastien.mendes@he-arc.ch>
2023-11-14 08:25:04 +0100

    add coyote time/wall jump

--- Characters/player.gd
+++ Characters/player.gd
@@ -9,6 +9,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
 @onready var tile_map : TileMap = $"../TileMap"
+@onready var coyote_time = $CoyoteTime
+var wall_jump_remaining
 
 func _physics_process(delta):
 	if $MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id():
@@ -17,7 +19,7 @@ func _physics_process(delta):
 		animated_sprite.play()
 		# Add the gravity.
 		if not is_on_floor():
-			velocity.y += gravity * delta
+			velocity.y = move_toward(velocity.y, 980, gravity * delta)
 			if (velocity.y <= 0):
 				animated_sprite.animation = "jump"
 			else:
@@ -27,16 +29,34 @@ func _physics_process(delta):
 				animated_sprite.animation = "idle"
 			else:
 				animated_sprite.animation = "run"
-				
-		# Handle Jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
 
+		var was_on_floor = is_on_floor()
+		
+		if is_on_floor():
+			wall_jump_remaining = 1
+		
+		var acceleration
+		if !is_on_floor():
+			acceleration = 37.5
+		else:
+			acceleration = 100.0
+			
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("ui_left", "ui_right")
+		
+		# Handle Jump.
+		if Input.is_action_just_pressed("ui_accept") and velocity.y >= 0 and ((is_on_floor() or !coyote_time.is_stopped()) or (is_on_wall() and wall_jump_remaining)):
+			var new_speed = 0
+			if is_on_wall():
+				wall_jump_remaining = 1
+				new_speed = SPEED * direction * -2
+			velocity.x = new_speed
+			velocity.y = JUMP_VELOCITY
+		# print(velocity)
+		
 		if direction:
-			velocity.x = direction * SPEED
+			velocity.x = move_toward(velocity.x, direction * SPEED, acceleration)
 			
 			if (velocity.x < 0):
 				animated_sprite.flip_h = true
@@ -44,7 +64,7 @@ func _physics_process(delta):
 				animated_sprite.flip_h = false
 			
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, acceleration)
 			
 		var tile_map_pos = tile_map.local_to_map(Vector2i(position.x, position.y))
 
@@ -57,6 +77,9 @@ func _physics_process(delta):
 				kill()
 
 		move_and_slide()
+		
+		if was_on_floor && !is_on_floor():
+			coyote_time.start()
 
 func kill():
 	velocity.x = 0
