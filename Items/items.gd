3a555affc648269f199bd08c4fe444b3bc5e8bf5
Elisa Goloviatinski <elisa.goloviatinski@he-arc.ch>
2023-11-28 09:29:46 +0100

    Item deletion implemented

--- Items/items.gd
+++ Items/items.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+@export var this_scene : PackedScene
+@onready var object_cursor = get_node("/root/Level")
+@onready var cursor_sprite = object_cursor.get_node("Sprite")
+
+var inside = false
+
 func _mouse_enter():
-	if Input.is_action_pressed("click"):
-		queue_free()
+	inside = true
+
+func _mouse_exit():
+	inside = false
+
+func _input(event):
+	var bloc_coord = get_node("/root/Level").bloc_coord
+	var erase_text = get_node("/root/Level").erase_text
+	
+	if Input.is_action_pressed("click") and inside:
+		if (bloc_coord == erase_text):
+			queue_free()
