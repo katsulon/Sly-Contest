7bb55010095fd4370f3eb2dd911758c2698108c6
Eddy Naddeo <eddy@naddeo.ch>
2023-12-05 10:09:07 +0100

    File structure

---
+++ Game/Items/Saw.gd
@@ -0,0 +1,8 @@
+extends "res://Game/Items/items.gd"
+
+func _ready():
+	get_node("Sprite").play("on")
+
+func _on_body_entered(body):
+	if body.has_method("kill"):
+		body.kill()
--- Items/Saw.gd
+++
@@ -1,8 +0,0 @@
-extends "res://Items/items.gd"
-
-func _ready():
-	get_node("Sprite").play("on")
-
-func _on_body_entered(body):
-	if body.has_method("kill"):
-		body.kill()
